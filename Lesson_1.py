d6d2ba713da92c32a7a90fdba824dbf46efba949
umerlearner-debug <umerlearner@gmail.com>
2026-03-01 11:02:10 +0500

    print statement

---
+++ Lesson_1.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello world ")
+
+# Printing my name
+print("My name is 'Muhammad Umar'. ")
+
+# Printing a fun fact about myself
+print("I love to playing soccer.")
+
+# Performing and creating a simple calculation
+print("The sum of 5 + 3 is: ", 5+3 )
