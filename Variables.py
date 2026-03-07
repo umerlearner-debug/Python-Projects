d4780dae9ff233ba7cc378361c7ce8aabcf71662
umerlearner-debug <umerlearner@gmail.com>
2026-03-07 17:07:32 +0500

    print

---
+++ Variables.py
@@ -0,0 +1,15 @@
+# Creating different variables
+x = 5
+y = "john"
+
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+# UserInput
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWel-come to codingal.")
+
