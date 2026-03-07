10662e3304cd62cc5c538869cbd4f15e9b69e342
umerlearner-debug <umerlearner@gmail.com>
2026-03-07 16:56:21 +0500

    print

---
+++ Lesson_2.py
@@ -0,0 +1,14 @@
+# Print a sentence
+print("Wel-come to the world of Programming! ")
+
+# Print a number
+print(4)
+
+# Next line after print
+print("Hello Python \n")
+
+# Pass multiple arguments for printing
+print("Hello", 5)
+
+# End argument of print statement
+print("Wel-come to ", end= "*")
