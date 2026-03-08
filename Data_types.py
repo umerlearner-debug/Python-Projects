7ed65c5fcaccf4b3cd80326772eef67b779eb4c3
umerlearner-debug <umerlearner@gmail.com>
2026-03-08 10:41:20 +0500

    print statement

---
+++ Data_types.py
@@ -0,0 +1,12 @@
+# Let's check the different data types of different values.
+a = 5
+print("A is a data type of: ", type(a))
+
+b = "Codingal"
+print("B is a data type of: ", type(b))
+
+c = True
+print("C is a data type of: ", type(c))
+
+d = 12.5
+print("C is a data type of: ", type(d))
