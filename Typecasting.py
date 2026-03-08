bb22cc788befa6b7f094abfe6bd5f7d9bdd5a7c5
umerlearner-debug <umerlearner@gmail.com>
2026-03-08 10:59:51 +0500

    print statement

---
+++ Typecasting.py
@@ -0,0 +1,28 @@
+# Assigning different values.
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing different data types and their data type.
+print("Name: ", name)
+print("Data type of name is: ", type(name))
+
+print("Age: ", age)
+print("Data type of age is: ", type(age))
+
+print("Is_student: ", is_student)
+print("Data type of is_student: ", type(is_student))
+
+print("weight: ", weight)
+print("Data type of weight is: ", type(weight))
+
+# Typecasting to print a data type of variable.
+print("\n After typecasting....")
+age = str(age)
+print(age)
+print("Data type of age is: ", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data type of weight is: ", type(weight))
