84d5e960590ca8e393317f2bc8577958e5131ca6
umerlearner-debug <umerlearner@gmail.com>
2026-03-14 16:56:19 +0500

    print

---
+++ Average.py
@@ -0,0 +1,14 @@
+# storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+# Finding the total of trees.
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all the 5 trees is: ", sum)
+
+# Finding the average of trees.
+average = sum/5
+print("The average of all trees is: ", average)
