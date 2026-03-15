c044faa809c2b4acd96b91637595a4787cb48c17
umerlearner-debug <umerlearner@gmail.com>
2026-03-15 11:29:34 +0500

    print

---
+++ Even_number.py
@@ -0,0 +1,9 @@
+number = int(input("Enter number to check: "))
+print("number to be checked: ", number)
+
+if number%2==0:
+    print("This is an even number.")
+
+else:
+    print("This is an odd number.")
+        
---
+++ Profit_loss.py
@@ -0,0 +1,10 @@
+actual_cost = float(input("Please enter the actual product price: "))
+sale_amount = float(input("Please enter the sale amount: "))
+
+if (sale_amount > actual_cost):
+
+    amount = sale_amount - actual_cost
+    print("Total Profit = {0}".format(amount))
+
+else:
+    print("No profit !!!!")
