36627a5bd22340e75d947274f3324c78cbe76059
umerlearner-debug <umerlearner@gmail.com>
2026-03-15 11:22:22 +0500

    print

---
+++ Number_greater.py
@@ -0,0 +1,10 @@
+i = int(input("Enter a number: "))
+if (i > 15):
+    print("i is smaller than 15.")
+    print("I am in if block.")
+
+else: 
+    print("i is greater than 15.")
+    print("I am in else block.")
+
+print("I am not in if block and not in else block.")
