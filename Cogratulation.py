eddacf96ec3b0c54b40903fa387de9b32806a126
umerlearner-debug <umerlearner@gmail.com>
2026-03-16 13:48:44 +0500

    print_statement

---
+++ Cogratulation.py
@@ -0,0 +1,8 @@
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Create a congratulations message using string operations
+message = "Congratulations " + name + "! You did a great job!"
+
+# Print the message
+print(message)
