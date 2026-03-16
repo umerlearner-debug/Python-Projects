ac6b34685aa42de41479e6ba3713230b786098b5
umerlearner-debug <umerlearner@gmail.com>
2026-03-16 13:43:39 +0500

    print_statement

---
+++ Square_root.py
@@ -0,0 +1,10 @@
+import math
+
+# Ask the user to enter a number
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+result = math.sqrt(num)
+
+# Show the result
+print("The square root of", num, "is", result)
