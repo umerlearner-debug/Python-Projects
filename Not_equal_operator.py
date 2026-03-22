adf6cec97334f99b244446cddcaf8a7cfb105209
umerlearner-debug <umerlearner@gmail.com>
2026-03-22 11:27:12 +0500

    print

---
+++ Not_equal_operator.py
@@ -0,0 +1,25 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "Python"
+b = "coding"
+
+if a != b:
+    print(a, "and" ,b, "are different.")
+
+a = 4
+b = 5
+
+if (a==b) != (b==5):
+    print("hello")
+
+a = int(input("Enter a number: "))
+
+if a%2 != 0:
+    print(a, "is not even number.")
+    
+
