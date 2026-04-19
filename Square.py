b693cca77424fd199f6af2cb9872284516a7ad84
umerlearner-debug <umerlearner@gmail.com>
2026-04-19 09:16:32 +0500

    square

---
+++ Square.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("Navy")
+
+board = turtle.Turtle()
+board.pencolor("aliceblue")
+
+
+board.forward(100) # draw base
+
+
+board.left(100)
+
+board.forward(100)
+
+
+board.right(100)
+board.penup()
