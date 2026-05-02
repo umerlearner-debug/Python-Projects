d7145dee3e37f6e630b8e67e2d3d9448630bba80
umerlearner-debug <umerlearner@gmail.com>
2026-05-02 17:13:13 +0500

    multiple

---
+++ Multiple.py
@@ -0,0 +1,18 @@
+try:
+    num1,num2 = eval(input("Enter two numbers separated by comma: "))
+    result = num1 / num2
+    print("Result is ", result)
+
+except ZeroDivisionError:
+    print("Division by zero is error !!")
+
+except SyntaxError:
+    print("Comma is missing. Enter numbers separated by comma like: 1, 2.")
+
+except:
+     print("Wrong input")
+
+else:
+    print("No exceptions.")
+finally:
+    print("This will execute no matter what.")
