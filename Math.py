9189ab5a9e0c51851d5965427631fc081c102f77
umerlearner-debug <umerlearner@gmail.com>
2026-05-09 23:35:16 +0500

    math

---
+++ Math.py
@@ -0,0 +1,8 @@
+import math 
+deg = float(input("Enter a decimal number: "))
+rad = math.radians(deg)
+sin_val= math.sin(rad)
+sin_val= math.cos(rad) 
+sin_val= math.tan(rad)
+print(sin_val) 
+3-Times
