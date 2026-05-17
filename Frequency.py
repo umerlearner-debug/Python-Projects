e51b923ca9d13182307ef4f6443eb1d45173e25b
umerlearner-debug <umerlearner@gmail.com>
2026-05-17 11:36:24 +0500

    all

---
+++ Frequency.py
@@ -0,0 +1,11 @@
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+print("The original dictionary: " + str(test_dict))
+
+k = 2
+res = 0
+
+for key in test_dict:
+    if test_dict[key] == k:
+        res = res + 1
+        
+print("Frequency of k is: " + str(res))
---
+++ country_code.py
@@ -0,0 +1,11 @@
+country_code = {'India' : '0091',
+                'Australia' : '0025',
+                'Nepal' : '00977'}
+
+print("country code for india -")
+print(country_code.get('India',"not found"))
+
+print("country code for japan -")
+print(country_code.get('japan',"not found"))
+
+
