9f027f98b796c9d1b5b397da9ecf2f08d4d66cec
umerlearner-debug <umerlearner@gmail.com>
2026-05-23 16:55:24 +0500

    set

---
+++ operations_on_set.py
@@ -0,0 +1,21 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1.2,"hello",(1,2,3)}
+print(my_set)
+
+my_set = {1,2,3,4,3,2}
+print(my_set)
+
+num_set = set([0,1,2,3])
+print("Original set: ")
+print(num_set)
+num_set.pop
+
+print("After removing the first the said set: ")
+print(num_set,"\n")
+
+
+
+
+
