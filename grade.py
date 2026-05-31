70a219655a224f331a290017e37609ba841f8d1f
umerlearner-debug <umerlearner@gmail.com>
2026-05-31 11:31:25 +0500

    grades

---
+++ grade.py
@@ -0,0 +1,13 @@
+student_marks = {"student1": 90, "student2": 85, "student3": 75, "student4": 74, "student5": 70 }
+for i in student_marks.values():
+
+    average = 394/5
+    print(average)
+
+max_average = max(student_marks)
+print(max_average)
+min_average = min(student_marks)
+print(min_average)
+student_marks.get()
+
+student_name = input("Enter the name of student that you want to see the grades of: ")
