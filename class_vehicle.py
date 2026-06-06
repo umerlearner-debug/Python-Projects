1052d60699ae1e3f4d47b8fbc675eff1384c5504
umerlearner-debug <umerlearner@gmail.com>
2026-06-06 16:58:57 +0500

    vehicle

---
+++ class_vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+model_x = Vehicle(240, 18)
+
+print("model max speed:", model_x.max_speed)
+print("Model mileage:", model_x.mileage)
