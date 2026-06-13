631e3a78bb8f391c100029ddc98fc915d9788316
umerlearner-debug <umerlearner@gmail.com>
2026-06-13 16:52:20 +0500

    bus

---
+++ Is_this_bus.py
@@ -0,0 +1,12 @@
+class vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    
+class Bus(vehicle):
+    pass
+
+School_bus = Bus("School value", 188, 12)
+print("Vehicle Name", School_bus.name, "Speed", School_bus. max_speed, "Mileage", School_bus.mileage)
