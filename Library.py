806dfd66d0907193ee4d3f9160c3c0f38ec511e6
umerlearner-debug <umerlearner@gmail.com>
2026-06-21 11:37:20 +0500

    Books

---
+++ Library.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        is_borrowd = False
+    
+    def borrow(self):
+
+
+
+    def return_book(self):
+
+
+
+x = Book("abc", "Charles")
+
+y = Book("DEF", "John")
+
+x.borrow()
+y.return_book()
+
+
+
+
+
+        
