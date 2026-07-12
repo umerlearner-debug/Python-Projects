accef99f49ad54827e4955617b12535314fedec2
umerlearner-debug <umerlearner@gmail.com>
2026-07-12 11:17:26 +0500

    sprite

--- sprite_collision.py
+++ sprite_collision.py
@@ -19,7 +19,7 @@ class Sprite(pygame.sprite.Sprite):
         self.image = pygame.Surface([width, height])
         self.image.fill(pygame.Color('dodger blue'))
 
-        pygame.draw.rect(self.image, color, pygame.Rect(0,0, width, height))
+        pygame.draw.rect(self.image, colour, pygame.Rect(0,0, width, height))
 
         self.rect = self.image.get_rect()
     
@@ -36,10 +36,46 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sprite Collision")
 all_sprites = pygame.sprite.Group()
 
-sprite1 = Sprite(pygame.color('black'), 20, 30)
-sprite1.rect.x, sprite1.rect.y = random.randint(
-    0, SCREEN_WIDTH - sprite1.rect.width, random.randint(
-        0, SCREEN_HEIGHT - sprite1.rect.height, random.randint
-    ))
+sprite1 = Sprite(pygame.Color('black'), 20, 30)
+sprite1.rect.x, sprite1.rect.y = random.randint(0, SCREEN_WIDTH - sprite1.rect.width), random.randint(0, SCREEN_HEIGHT - sprite1.rect.height)
 
 all_sprites.add(sprite1)
+
+sprite2 = Sprite(pygame.Color('red'), 20, 30)
+sprite2.rect.x, sprite2.rect.y = random.randint(0, SCREEN_WIDTH - sprite2.rect.width), random.randint(0, SCREEN_HEIGHT - sprite2.rect.height)
+
+all_sprites.add(sprite2)
+
+running, won = True, False
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K-x):
+            running = False
+
+
+        if not won:
+            keys = pygame.key.get_pressed()
+            x_change = (keys[pygame.K_RIGHT] -keys[pygame.K_LEFT]) * MOVEMENT_SPEED
+            y_change = (keys[pygame.K_DOWN] -keys[pygame.K_UP]) * MOVEMENT_SPEED
+            sprite1.move(x_change, y_change)
+
+            if sprite1.rect.colliderect(sprite2.rect):
+                all_sprites.remove(sprite2)
+                won = True
+        
+        screen.blit(background_image, (0,0))
+        all_sprites.draw(screen)
+
+
+        if won:
+            win_text = font.render("You win!", True, pygame.Color('black'))
+            screen.blit(win_text, ((SCREEN_WIDTH - win_text.get_width())) // 2, (SCREEN_HEIGHT - win_text.get_height()) // 2  )
+
+        pygame.display.flip()
+        clock.tick(90)
+
+pygame.quit()
+
+
