bca8fc2e897432b95df0fef9b8409fd14bba3a0d
umerlearner-debug <umerlearner@gmail.com>
2026-08-22 17:26:28 +0500

    denomination

--- denomination_calculator.py
+++ denomination_calculator.py
@@ -1,15 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
 
-from PIL import Image, ImageTk()
+from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Denomination Counter")
-root.configure("Light Blue")
+root.configure(bg = "light blue")
 root.geometry("650x400")
 
-upload = Image.open("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0C04mSQer5MURkHpU3y3khpZeaObc1pPqCwbZSykmKg&s=10")
-upload = upload.resize("300, 300")
+upload = Image.open("app_img.jpg")
+upload = upload.resize((300, 300))
 image = ImageTk.PhotoImage(upload)
 
 label = Label(root, image=image, bg ="Light Blue")
@@ -34,4 +34,69 @@ def topwin():
     top = Toplevel()
     top.title("Denomination calculator")
     top.configure(bg="light grey")
-    
+    top.geometry("600x350+50+50")
+    label = Label(top, text="Enter total amount", bg="light grey", entry = Entry(top))
+    lbl = Label(top, text="Here are number of notes for each denomination.", bg="Light grey")
+
+    l1 = Label(top, text=2000, bg="Light grey")
+    l2 = Label(top, text=500, bg="Light grey")
+    l3 = Label(top, text=100, bg="Light grey")
+
+    t1 = Entry(top)
+    t2 = Entry(top)
+    t3 = Entry(top)
+
+    def Calculator():
+        try:
+            amount = int(Entry.get())
+
+            note2000 = amount // 2000 
+            amount %= 2000
+
+            note500 = amount // 500
+            amount %= 500
+
+            note100 = amount // 100
+
+            t1.delete(0, END)
+            t2.delete(0, END)
+            t3.delete(0, END)
+
+            t1.insert(END, str(note2000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
+
+
+        except ValueError:
+            messagebox.showerror("Error", "Please enter a valid number")
+
+
+    btn = Button (
+        top,
+        text ="calculator",
+        command = Calculator,
+        bg = "brown",
+        fg = "White"
+
+                )
+
+    label.place(x=230, y=50)
+    entry.place(x=200, y=50)
+    btn.place(x=240, y=120)
+
+    lbl.place(x=140, y=170)
+
+    l1.place(x=180, y=200)
+    l2.place(x=180, y=230)
+    l3.place(x=140, y=260)
+
+    t1.place(x=270, y=200)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
+
+    top.mainloop()
+
+
+root.mainloop()
+
+
